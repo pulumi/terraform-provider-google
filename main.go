e133e3a44b774480cbcd8c8d43331ac5418e77e4
Sean Gillespie <sean@pulumi.com>
2018-06-06 18:18:51 -0700

    Keep the same package identity

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/pulumi/terraform-provider-google/google"
+	"github.com/terraform-providers/terraform-provider-google/google"
 )
 
 func main() {
